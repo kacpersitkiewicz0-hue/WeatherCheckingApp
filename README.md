6d8617a9fe303b3710a84c626c370e37f52e67c8
kacpersitkiewicz0-hue <kacper.sitkiewicz0@gmail.com>
2026-07-07 13:52:12 +0200

    Adding css to this project along with some other stuff

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WeatherCheckingApp
+
+Me -70% of project
+Gemini -30% of project
--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const apiKey = "5220bbe3a1e28b41d3cf03284f6d60e6";
 
-// 1. Łapiemy elementy z HTML
 const form = document.querySelector("form");
 const cityInput = document.querySelector("input");
-
+const weatherIcon = document.getElementById("weatherIcon");
 const cityName = document.getElementById("cityName");
 const temperature = document.getElementById("temp");
 const description = document.getElementById("desc");
@@ -25,6 +24,8 @@ form.addEventListener("submit", function (event) {
       cityName.textContent = data.name;
       temperature.textContent = Math.round(data.main.temp) + "°C";
       description.textContent = data.weather[0].description;
+      const iconCode = data.weather[0].icon;
+      weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
     })
     .catch((error) => {
       console.log("Błąd połączenia z serwerem:", error);
